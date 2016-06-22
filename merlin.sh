1690758d1f47d4e7197a84f0b80c6d3bf65a3b6f
柳啸虎 <liuxh3388@gmail.com>
2016-06-22 20:07:15 +0800

    Update merlin.sh

--- merlin.sh
+++ merlin.sh
@@ -4,70 +4,67 @@ export PATH
 
 clear
 echo "#############################################################"
-echo "#     梅林路由专用     nvpproxy安装+设置openvpn还是得你自己设置   r6300v2已测试        #"
-echo "#     梅林路由专用     openvpn部分还是得你自己设置        #"
-echo "#     梅林路由专用     本脚本仅把命令部分做成一键,带防火墙配置        #"
+echo "#     梅林路由专用     nvpproxy安装+设置   r6300v2已测试        #"
+echo "#      openvpn部分还是得你自己设置        #"
+echo "#      本脚本仅把命令部分做成一键,带防火墙配置        #"
 echo "#############################################################"
 echo ""
 
-# Make sure only root can run our script
-function rootness(){
-if [[ $EUID -ne 0 ]]; then
-   echo "错误:请使用root权限!" 1>&2
-   exit 1
-fi
-}
-
-# Pre-installation settings
 function pre_install(){
-     fi
-    #Set shadowsocks-libev config password
-    echo "请输入密码 for shadowsocks-libev:"
-    read -p "(默认密码: wxliuxh):" shadowsockspwd
-    [ -z "$shadowsockspwd" ] && shadowsockspwd="wxliuxh"
-    echo ""
-    echo "---------------------------"
-    echo "密码 = $shadowsockspwd"
-    echo "---------------------------"
-    echo ""
-    #Set shadowsocks-libev config port
+    #Set merlin-openvpn config port
     while true
     do
-    echo -e "请输入端口 for shadowsocks-libev [1-65535]:"
-    read -p "(默认端口: 6688):" shadowsocksport
-    [ -z "$shadowsocksport" ] && shadowsocksport="6688"
-    expr $shadowsocksport + 0 &>/dev/null
+    echo -e "请输入端口 (如果不懂请直接回车)  [1-65535]:"
+    read -p "(默认端口: 6688):" merlinport
+    [ -z "$merlinport" ] && merlinport="6688"
+    expr $merlinport + 0 &>/dev/null
     if [ $? -eq 0 ]; then
-        if [ $shadowsocksport -ge 1 ] && [ $shadowsocksport -le 65535 ]; then
+        if [ $merlinport -ge 1 ] && [ $merlinport -le 65535 ]; then
             echo ""
             echo "---------------------------"
-            echo "端口 = $shadowsocksport"
+            echo "端口 = $merlinport"
             echo "---------------------------"
             echo ""
             break
         else
-            echo "Input error! Please input correct numbers."
+            echo "输入错误！请输入正确的端口数字."
         fi
     else
-        echo "Input error! Please input correct numbers."
+        echo "输入错误！请输入正确的端口数字."
     fi
     done
-	
-
-# Config shadowsocks
-function config_shadowsocks(){
-    if [ ! -d /root/supervisor ];then
-        mkdir /root/supervisor
-		mkdir /root/supervisor/log
+    get_char(){
+        SAVEDSTTY=`stty -g`
+        stty -echo
+        stty cbreak
+        dd if=/dev/tty bs=1 count=1 2> /dev/null
+        stty -raw
+        stty echo
+        stty $SAVEDSTTY
+    }
+    echo ""
+    echo "按任意键继续安装...或 按 Ctrl+C 取消安装"
+    char=`get_char`
+    # Get IP address
+    echo "Getting Public IP address, Please wait a moment..."
+    IP=$(curl -s -4 icanhazip.com)
+    if [[ "$IP" = "" ]]; then
+        IP=`curl -s -4 ipinfo.io/ip`
     fi
-    cat > /root/supervisor/ss-${shadowsocksport}.conf<<-EOF
-[program:sslibev_${shadowsocksport}]
-command=/usr/local/bin/ss-server -u -p ${shadowsocksport} -k ${shadowsockspwd} -m chacha20
-autostart=true
-autorestart=true
-user=root
-startsecs=10
-startretries=36
+    echo -e "Your main public IP is\t\033[32m$IP\033[0m"
+    echo ""
+    #Current folder
+    cur_dir=`pwd`
+    cd $cur_dir
+}
+
+# Config merlin
+function config_merlin(){
+	cat > /jffs/scripts/init-start<<-EOF
+#!/bin/sh
+sleep 80
+iptables -I INPUT -p tcp --dport ${merlinport} -j ACCEPT
+start-stop-daemon -S -q -b -m -p /tmp/var/npvproxy.pid -x /jffs/nvpproxy -- -port=${merlinport} -proxy=127.0.0.1:1194
 EOF
 }
 
@@ -78,8 +75,8 @@ function install(){
     echo ""
     echo "端口添加成功!"
     echo -e "服务器IP: \033[41;37m ${IP} \033[0m"
-    echo -e "远程端口: \033[41;37m ${shadowsocksport} \033[0m"
-    echo -e "你的密码: \033[41;37m ${shadowsockspwd} \033[0m"
+    echo -e "远程端口: \033[41;37m ${merlinport} \033[0m"
+    echo -e "你的密码: \033[41;37m ${merlinpwd} \033[0m"
     #echo -e "本地IP: \033[41;37m 127.0.0.1 \033[0m"
     echo -e "本地端口: \033[41;37m 1080 \033[0m"
     echo -e "加密方法: \033[41;37m chacha20 \033[0m"
@@ -89,23 +86,23 @@ function install(){
     exit 0
 }
 
-# Uninstall Shadowsocks-libev
-function uninstall_shadowsocks_libev(){
+# Uninstall merlin-openvpn
+function uninstall_merlin_libev(){
 	cd /root/supervisor
 	echo "---------------------------"
 	ls
 	echo "---------------------------"
 	 while true
     do
-    echo -e "请输入端口 for shadowsocks-libev [1-65535]:"
-    read -p "(默认端口: 16888):" shadowsocksport
-    [ -z "$shadowsocksport" ] && shadowsocksport="16888"
-    expr $shadowsocksport + 0 &>/dev/null
+    echo -e "请输入端口 for merlin-openvpn [1-65535]:"
+    read -p "(默认端口: 16888):" merlinport
+    [ -z "$merlinport" ] && merlinport="16888"
+    expr $merlinport + 0 &>/dev/null
     if [ $? -eq 0 ]; then
-        if [ $shadowsocksport -ge 1 ] && [ $shadowsocksport -le 65535 ]; then
+        if [ $merlinport -ge 1 ] && [ $merlinport -le 65535 ]; then
             echo ""
             echo "---------------------------"
-            echo "端口 = $shadowsocksport"
+            echo "端口 = $merlinport"
             echo "---------------------------"
             echo ""
             break
@@ -117,17 +114,16 @@ function uninstall_shadowsocks_libev(){
     fi
     done
 	cd /root
-	rm -rf /root/supervisor/ss-${shadowsocksport}.conf
+	rm -rf /root/supervisor/ss-${merlinport}.conf
 	service supervisord restart
 	echo "端口配置删除成功!"
 }
 
-# Install Shadowsocks-libev
-function install_shadowsocks_libev(){
-    rootness
-    #pre_install
-   # config_shadowsocks
-   # install
+# Install merlin-openvpn
+function install_merlin_libev(){
+    pre_install
+    config_merlin
+    #install
 }
 
 # Initialization step
@@ -135,10 +131,10 @@ action=$1
 [ -z $1 ] && action=add
 case "$action" in
 add)
-    install_shadowsocks_libev
+    install_merlin_libev
     ;;
 del)
-    uninstall_shadowsocks_libev
+    uninstall_merlin_libev
     ;;
 *)
     echo "参数错误! [${action} ]"
